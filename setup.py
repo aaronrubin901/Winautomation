f60563117c96890fbe412319a2ee97a976b0140b
Aharon <aharonr@ravtech.co.il>
2017-01-02 15:49:43 +0200

    added toggle state

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='winautomation',
+    version='1.0.2',
+    description='Python realisation of basic Microsoft UI Automation functionality',
+    py_modules=['winautomation'],
+    requires=['comtypes']
+)
